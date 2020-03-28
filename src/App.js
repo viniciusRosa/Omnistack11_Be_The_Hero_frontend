9175b505242b7f814f015469c1cfec99943a8956
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-03-27 22:06:42 -0300

    init logon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
+
+import Logon from './pages/logon'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+    <Logon />
+    );
 }
 
 export default App;
---
+++ src/pages/logon/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css'
+
+function Logon() {
+  return (
+      <>
+      </>
+    );
+}
+
+export default Logon;
