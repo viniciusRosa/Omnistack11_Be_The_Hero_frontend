7b9976c1462296f7eb96d4a9250aaa9eef6fdae1
Vinicius Rosa <vinicius.rosa00@gmail.com>
2020-04-09 12:27:03 -0300

    new incidet page

--- src/pages/newIncident/index.js
+++ src/pages/newIncident/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiPower, FiTrash2 } from 'react-icons/fi'
+import { FiArrowLeft  } from 'react-icons/fi'
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg';
@@ -7,13 +7,35 @@ import heroesImg from '../../assets/heroes.png';
 import { Link } from 'react-router-dom';
 
 function NewIncident() {
-  return (
-    <>
-      <div className="newIncident-container">
-        
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="new-incident-container">
+				<div className="content">
+					<section>
+						<img src={logoImg} alt="Be the Hero" />
+
+						<h1>Cadastrar novo caso</h1>
+						<p>Descreva o caso.</p>
+						<Link className="back-link" to="/profile">
+							<FiArrowLeft size={16} color='#E02041' />
+              voltar para a home
+            </Link>
+					</section>
+
+					<form action="">
+						<input type="text" placeholder="Título do caso" />
+						<textarea placeholder="Descrição" />
+						<input type="text" placeholder="valor" />
+
+					
+
+						<button className="button" type="submit">Cadastrar</button>
+
+					</form>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default NewIncident;
